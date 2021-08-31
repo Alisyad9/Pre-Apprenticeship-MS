18a9cc2beca97323f4bab48c092e4df9912c7645
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-08-31 01:21:46 +0100

    test name changed

--- week8-9/learn-unit-testing/workshop/index.test.js
+++ week8-9/learn-unit-testing/workshop/index.test.js
@@ -1,5 +1,7 @@
-test("makeUrl should create the correct PokéAPI URL", () => {
+test("makeUrl", () => {
   equal(makeUrl("pikachu"), "https://pokeapi.co/api/v2/pikachu");
   equal(makeUrl("bulbasaur"), "https://pokeapi.co/api/v2/bulbasaur");
   equal(makeUrl("hitmonle"), "https://pokeapi.co/api/v2/hitmonlee");
 });
+
+test("searchParamsToObject");
