1ee252dc7bf528cc99c2d09904c06b4df8226e9b
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-08-31 02:59:39 +0100

    initial test

---
+++ youTubeTesting/test.js
@@ -0,0 +1,17 @@
+// const names ={
+//     person1='james',
+//     person2='ale'
+// };
+const car = { type: "Fiat", model: "500", color: "white" };
+const people = ["james", "ali", "john"];
+function namesInObject(arrayOfcarnames) {
+  //     console.log(arrayOfcarnames.map((car) => car));
+  //   return arrayOfcarnames.map((car) => car);
+
+  // return arrayOfcarnames.map(function (car) {})
+  return arrayOfcarnames.map(function (cars) {
+    return cars.type;
+  });
+}
+
+namesInObject(car);
