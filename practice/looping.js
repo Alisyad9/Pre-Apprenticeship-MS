7420be5540d766d7d841195d4e0195b4c4010e71
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-09-05 20:34:00 +0100

    loops

--- practice/looping.js
+++ practice/looping.js
@@ -1,17 +1,17 @@
-let array = [1, -2, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let array = [1, -2, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-biggestValue = 0;
-smallestValue = Infinity;
+// biggestValue = 0;
+// smallestValue = Infinity;
 
-array.forEach((element) => {
-  console.log(element);
-});
+// array.forEach((element) => {
+//   console.log(element);
+// });
 
-array.map((element) => console.log(element));
+// array.map((element) => console.log(element));
 
-const result = array.filter((element) => element > 0);
+// const result = array.filter((element) => element > 0);
 
-console.log(result);
+// console.log(result);
 
 for (const iterator of array) {
   console.log(iterator);
